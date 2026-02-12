49be5b00cd92c36938ee4552b19f272a799da11b
Abhishek Sharma <chirag.sharma1008cs@gmail.com>
2026-02-12 20:32:24 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ml-automater",  # PyPI par 'automater' shayad taken ho
+    version="0.1.0",
+    author="Abhishek Sharma",
+    description="An all-in-one automated ML pipeline for feature engineering, optimization, and evaluation.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/automater",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "category_encoders",
+        "optuna",
+        "catboost",
+        "xgboost",
+        "lightgbm",
+        "plotly",
+        "kaleido",
+        "openpyxl"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
