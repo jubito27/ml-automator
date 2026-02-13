d76584880247cd06bc2f43a3716902576cb162fb
Abhishek Sharma <chirag.sharma1008cs@gmail.com>
2026-02-13 09:47:31 +0530

    Update __init__.py

--- ml-automator/__init__.py
+++ ml-automator/__init__.py
@@ -1,3 +1,26 @@
+# Version definition
+__version__ = "0.1.0"
+
+# Module se classes aur functions ko expose karna
 from .feature_engineering import FeatureEvaluation
-from .models_optimizer import randomforest, adaboost, catboost, xgboost, lightgbm, gradientboosting
+from .models_optimizer import (
+    randomforest, 
+    adaboost, 
+    catboost, 
+    xgboost, 
+    lightgbm, 
+    gradientboosting
+)
 from .trainer import Trainer
+
+# Ye define karta hai ki 'from automater import *' karne par kya kya aayega
+__all__ = [
+    'FeatureEvaluation',
+    'Trainer',
+    'randomforest',
+    'adaboost',
+    'catboost',
+    'xgboost',
+    'lightgbm',
+    'gradientboosting'
+]
